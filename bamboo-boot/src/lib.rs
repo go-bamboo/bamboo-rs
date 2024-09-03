d8101a4261c1b40c92ea623fd443e53fc13fdce7
emberfarkas <emberfarkas@hotmail.com>
2024-09-03 14:15:49 +0800

    feat: init

---
+++ bamboo-boot/src/lib.rs
@@ -0,0 +1,18 @@
+pub mod app;
+pub mod builder;
+pub mod plugin;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
