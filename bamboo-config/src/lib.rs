ace26b45bbe353fc925fa14e09bbe9e91b3f1b35
emberfarkas <emberfarkas@hotmail.com>
2024-09-05 10:43:57 +0800

    feat: config

--- bamboo-config/src/lib.rs
+++ bamboo-config/src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+//! [bamboo](https://go-bamboo.github.io/)
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use config::*;
