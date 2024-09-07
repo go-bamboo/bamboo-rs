0f917068f85b3ee81960beed16235af7e3716f1e
emberfarkas <emberfarkas@hotmail.com>
2024-09-07 16:35:23 +0800

    feat: tower

--- bamboo-tower/src/lib.rs
+++ bamboo-tower/src/lib.rs
@@ -1,3 +1,60 @@
+use std::{
+    pin::Pin,
+    task::{Context, Poll},
+};
+use tower_layer::Layer;
+use tower_service::Service;
+use tonic::{body::BoxBody, transport::Server, Request, Response, Status};
+
+#[derive(Debug, Clone, Default)]
+struct MyMiddlewareLayer {}
+
+impl<S> Layer<S> for MyMiddlewareLayer {
+    type Service = MyMiddleware<S>;
+
+    fn layer(&self, service: S) -> Self::Service {
+        MyMiddleware { inner: service }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct MyMiddleware<S> {
+    inner: S,
+}
+
+type BoxFuture<'a, T> = Pin<Box<dyn std::future::Future<Output = T> + Send + 'a>>;
+
+impl<S> Service<hyper::Request<BoxBody>> for MyMiddleware<S>
+    where
+        S: Service<hyper::Request<BoxBody>, Response = hyper::Response<BoxBody>>
+        + Clone
+        + Send
+        + 'static,
+        S::Future: Send + 'static,
+{
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
+
+    fn call(&mut self, req: hyper::Request<BoxBody>) -> Self::Future {
+        // This is necessary because tonic internally uses `tower::buffer::Buffer`.
+        // See https://github.com/tower-rs/tower/issues/547#issuecomment-767629149
+        // for details on why this is necessary
+        let clone = self.inner.clone();
+        let mut inner = std::mem::replace(&mut self.inner, clone);
+
+        Box::pin(async move {
+            // Do extra async work here...
+            let response = inner.call(req).await?;
+
+            Ok(response)
+        })
+    }
+}
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
